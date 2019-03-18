af8d14087a47280eec8aa552521fbe968e25b300
Sebastian Veigli <seb.veigli@gmail.com>
2019-03-18 17:48:01 +0100

    Add error definition module

---
+++ src/errors/errors.py
@@ -0,0 +1,7 @@
+class InvalidConsoleException(Exception):
+    """
+    Thrown when a new Player is attempted to be created by the bot ->
+
+    If the user doesn't specify a valid console, this will be thrown.
+    """
+    pass
