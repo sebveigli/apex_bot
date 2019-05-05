c24b1bb52ddaeac9cfdf2bbe22dbb920fa3d7059
Sebastian Veigli <seb.veigli@gmail.com>
2019-05-06 00:29:56 +0200

    Remove all previous files for refactor

--- src/errors/errors.py
+++
@@ -1,7 +0,0 @@
-class InvalidConsoleException(Exception):
-    """
-    Thrown when a new Player is attempted to be created by the bot ->
-
-    If the user doesn't specify a valid console, this will be thrown.
-    """
-    pass
