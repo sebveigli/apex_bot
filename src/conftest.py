ab209f3f56ca34853b05cc5ab09fc850d0f3d2cb
Sebastian Veigli <seb.veigli@gmail.com>
2019-06-05 22:25:00 +0200

    Add fixture to mimmick API response

--- src/conftest.py
+++ src/conftest.py
@@ -12,3 +12,34 @@ def mock_message_dispatcher():
             return val
 
     return MockMessageDispatcher()
+
+@pytest.fixture(scope='session')
+def example_api_response():
+    class GetResponse:
+        def get_response(self):
+            return {
+                "legends": {
+                    "selected": {
+                        "Pathfinder": {
+                            "damage": 1000,
+                            "ImgAssets": {"icon": "http://foobar.com"},
+                        }
+                    },
+                    "all": {
+                        "Pathfinder": {
+                            "data": {"damage": 1000},
+                            "ImgAssets": {"icon": "http://foobar.com"},
+                        },
+                        "Bangalore": {
+                            "ImgAssets": {"icon": "http://foobar.com"},
+                        }
+                    }
+                },
+                "realtime": {},
+                "total": {"kd": 2},
+                "global": {},
+                "mozambiquehere_internal": {},
+                "event": {},
+            }
+
+    return GetResponse()
